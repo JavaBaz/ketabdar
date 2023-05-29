0919a9d30a77eea114c936db3d23763317323583
JavaBaz <Bazgir.civil@gmail.com>
2023-05-29 19:31:50 +0330

    Add fields of Book.java class and their setter and getter methods + Override toString method.

--- src/main/java/com/ketabdar/entity/Book.java
+++ src/main/java/com/ketabdar/entity/Book.java
@@ -5,5 +5,36 @@ public class Book {
     private String author;
     private int yearOfPublication;
 
+    public String getTittle() {
+        return tittle;
+    }
 
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "tittle='" + tittle + '\'' +
+                ", author='" + author + '\'' +
+                ", yearOfPublication=" + yearOfPublication +
+                '}';
+    }
 }
