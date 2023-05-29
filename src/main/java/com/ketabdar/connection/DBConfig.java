c064aff62bbfeba393ef800762969795f75ec13e
JavaBaz <Bazgir.civil@gmail.com>
2023-05-29 20:03:09 +0330

    Create connection package and its classes named DBConfig.java and DBConnection.java.

---
+++ src/main/java/com/ketabdar/connection/DBConfig.java
@@ -0,0 +1,7 @@
+package com.ketabdar.connection;
+
+public class DBConfig {
+    public static final String URL = "jdbc:postgresql://localhost:5432/ketabdar";
+    public static final String USER_NAME = "postgres";
+    public static final String PASSWORD = "1234";
+}
