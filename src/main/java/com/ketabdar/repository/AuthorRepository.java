cff5e391b3c00bef60f24269d43ccb7d66fa6b6a
JavaBaz <Bazgir.civil@gmail.com>
2023-06-01 02:43:58 +0330

    Add method save to AuthorRepository.java and AuthorService.java.

--- src/main/java/com/ketabdar/repository/AuthorRepository.java
+++ src/main/java/com/ketabdar/repository/AuthorRepository.java
@@ -1,4 +1,29 @@
 package com.ketabdar.repository;
 
+import com.ketabdar.connection.DBConnection;
+import com.ketabdar.entity.Author;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AuthorRepository {
+
+
+    public void save(Author author) {
+
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO author (first_name, last_name, age)" +
+                                                                                                               " VALUES (?,?,?);");
+            preparedStatement.setString(1, author.getFirstName());
+            preparedStatement.setString(2, author.getLastName());
+            preparedStatement.setInt   (3, author.getAge());
+            preparedStatement.execute();
+            preparedStatement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/ketabdar/service/AuthorService.java
+++ src/main/java/com/ketabdar/service/AuthorService.java
@@ -1,4 +1,13 @@
 package com.ketabdar.service;
 
+import com.ketabdar.entity.Author;
+import com.ketabdar.repository.AuthorRepository;
+
 public class AuthorService {
+    AuthorRepository authorRepository = new AuthorRepository();
+
+    public void save(Author author){
+        authorRepository.save(author);
+    }
+
 }
